8a5ef9a7f4616fd42611c7c72bb8f725c8ff7e65
dkeya <keyahdavid@gmail.com>
2025-07-02 23:33:01 +0300

    Remove ydata-profiling to fix deployment error

--- app.py
+++ app.py
@@ -25,7 +25,6 @@ from sklearn.model_selection import train_test_split
 from sklearn.metrics import mean_absolute_error, r2_score, mean_squared_error
 import logging
 import altair as alt
-from ydata_profiling import ProfileReport
 from streamlit_pandas_profiling import st_profile_report
 import dask.dataframe as dd
 import shap
